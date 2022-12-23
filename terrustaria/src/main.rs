79acdd9a1eb154005b35fbea4e5d833f00397f83
Toomimi <tmszglab@gmail.com>
2022-12-23 23:51:57 +0100

    Add basic map structure

--- terrustaria/src/main.rs
+++ terrustaria/src/main.rs
@@ -1,12 +1,8 @@
 use bevy::{math::Vec3Swizzles, prelude::*, time::FixedTimestep};
-use bevy_ecs_tilemap::{
-    prelude::*,
-};
-
+use bevy_ecs_tilemap::prelude::*;
 
 const TIME_STEP: f32 = 1.0 / 60.0;
 const BOUNDS: Vec2 = Vec2::new(1200.0, 640.0);
-
 
 fn main() {
     App::new()
@@ -24,6 +20,7 @@ fn main() {
                 .set(ImagePlugin::default_nearest()),
         )
         .add_plugin(TilemapPlugin)
+        .add_startup_system(spawn_map)
         .add_startup_system(setup)
         .add_system_set(
             SystemSet::new()
@@ -35,7 +32,6 @@ fn main() {
         .add_system(bevy::window::close_on_esc)
         .run();
 }
-
 
 #[derive(Component)]
 struct Player {
@@ -61,11 +57,17 @@ struct RotateToPlayer {
 // `Z` axis goes from far to near (`+Z` points towards you, out of the screen)
 // The origin is at the center of the screen.
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let background: Handle<Image> = asset_server.load("background.png");
+
+    // spawn background
+    // commands.spawn(SpriteBundle {
+    //     texture: background,
+    //     ..Default::default()
+    // });
+
     let ship_handle = asset_server.load("textures/simplespace/ship_C.png");
     let enemy_a_handle = asset_server.load("textures/simplespace/enemy_A.png");
     let enemy_b_handle = asset_server.load("textures/simplespace/enemy_B.png");
-
-    commands.spawn(Camera2dBundle::default());
 
     let horizontal_margin = BOUNDS.x / 4.0;
 
@@ -76,7 +78,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..default()
         },
         Player {
-            movement_speed: 500.0,                       // metres per second
+            movement_speed: 500.0,                  // metres per second
             rotation_speed: f32::to_radians(360.0), // degrees per second
         },
     ));
@@ -205,5 +207,81 @@ fn rotate_to_player_system(
 
         // rotate the enemy to face the player
         enemy_transform.rotate_z(rotation_angle);
+    }
+}
+
+fn transform_map(
+    size: &TilemapSize,
+    grid_size: &TilemapGridSize,
+    map_type: &TilemapType,
+    z: f32,
+) -> Transform {
+    let low = TilePos::new(0, 0).center_in_world(grid_size, map_type);
+    let high = TilePos::new(size.x - 1, size.y - 1).center_in_world(grid_size, map_type);
+
+    let diff = high - low;
+
+    Transform::from_xyz(-diff.x / 2., -diff.y, z)
+}
+
+//Spawn map entity
+fn spawn_map(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    #[cfg(all(not(feature = "atlas"), feature = "render"))] array_texture_loader: Res<
+        ArrayTextureLoader,
+    >,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
+
+    let map_size = TilemapSize { x: 32, y: 32 };
+
+    let tilemap_entity = commands.spawn_empty().id();
+
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    // Spawn the elements of the tilemap.
+    // Alternatively, you can use helpers::filling::fill_tilemap.
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::default();
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size,
+        // transform: Transform::from_xyz(-(map_size.x as f32)/2.0, -(map_size.y as f32), 0.0),
+        transform: transform_map(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
+
+    // Add atlas to array texture loader so it's preprocessed before we need to use it.
+    // Only used when the atlas feature is off and we are using array textures.
+    #[cfg(all(not(feature = "atlas"), feature = "render"))]
+    {
+        array_texture_loader.add(TilemapArrayTexture {
+            texture: TilemapTexture::Single(asset_server.load("tiles.png")),
+            tile_size,
+            ..Default::default()
+        });
     }
 }
